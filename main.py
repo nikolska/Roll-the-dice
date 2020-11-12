11a01ea60ddfa5a5b32f503612a898dd45b6c626
nikolska <nikolska1995@gmail.com>
2020-11-12 17:25:41 +0100

    doc-str

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import random
 
 
 def roll_the_dice(data):
+    '''
+    Function roll_the_dice takes a string, recognizes the type of dice, number of rolls,
+    modifier, and if the given string is incorrect, it will return the appropriate message,
+    otherwise it will simulate the rolls and return the result.
+    :data type: string
+    :return: result of the dice roll if data is correct, or error-message if not.
+    '''
     list_of_data = [i for i in data]
     first_part = 0
     last_part = 0
     action = ''
     number_of_dice = 0
     type_of_dice = 0
-    answer = []
+    answer = 0
     for i in list_of_data:
         if '+' in list_of_data:
             first_part = data.split('+')[0]
@@ -43,12 +50,12 @@ def roll_the_dice(data):
 
 cube_types = ['3', '4', '6', '8', '10', '12', '20', '100']
 
-print(roll_the_dice('2D10+10'))
-print(roll_the_dice('2D3'))
-print(roll_the_dice('D6'))
-print(roll_the_dice('D12-1'))
-print(roll_the_dice('D54-1'))
-print(roll_the_dice('5451'))
-print(roll_the_dice('D12/1'))
-print(roll_the_dice('D12/1'))
-print(roll_the_dice('5B68-1'))
+if __name__ == '__main__':
+    print(roll_the_dice('2D10+10'))
+    print(roll_the_dice('2D3'))
+    print(roll_the_dice('D6'))
+    print(roll_the_dice('D12-1'))
+    print(roll_the_dice('D54-1'))
+    print(roll_the_dice('5451'))
+    print(roll_the_dice('D12/1'))
+    print(roll_the_dice('5B68-1'))
